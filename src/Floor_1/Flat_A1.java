a3a007a9604a15a6fd70bba9673d2ad0d49661d0
mroygithub <mroygithub@gmail.com>
2022-10-15 22:58:41 +0530

    Third cmt

---
+++ src/Floor_1/Flat_A1.java
@@ -0,0 +1,19 @@
+package Floor_1;
+
+public class Flat_A1 {
+
+
+
+
+
+
+    public static void main(String args[]) throws Exception{
+
+        Flat_B1 obj_b1 = new Flat_B1();
+        System.out.println(obj_b1.bankInterestrate("SBI"));
+
+
+    }
+
+
+}
