00ea656e0800fbd0fa12b9aae96f629f6ae21237
mroygithub <mroygithub@gmail.com>
2022-10-16 20:46:40 +0530

    ArrayList

--- src/java_basic/learn_Array.java
+++ src/java_basic/learn_Array.java
@@ -4,14 +4,13 @@ public class learn_Array {
 
 
     public static void main(String args[]) throws Exception{
-        Learn_Int_Type_Array();
+        //Learn_Int_Type_Array();
+        Learn_String_Type_Array();
     }
 
 
 
     public static void Learn_Int_Type_Array(){
-
-
 
         // How to do Array declaration ...
 
@@ -31,8 +30,32 @@ public class learn_Array {
             System.out.println(num[i]);
         }
 
+    }
 
 
+    public static void Learn_String_Type_Array(){
+
+        // How to do Array declaration ...
+
+       String bankName[]  = new String[8];
+
+
+       // Array initialization ...
+
+        bankName[0] = "JPMC Bank";
+        bankName[1] = "SBI Bank";
+        bankName[2] = "PNB Bank";
+        bankName[3] = "HSBC Bank";
+        bankName[4] = "Duetsche Bank";
+        bankName[5] = "Credit Sue Bank";
+        bankName[6] = "ICICI Bank";
+        bankName[7] = "HDFC Bank";
+
+
+        for (int a = 0 ; a<bankName.length ; a ++){
+
+            System.out.println(bankName[a]);
+        }
 
 
 
